db8bb90349ed1672a5339ea74811bb6645eda8ef
awayisblue <awayisblue@qq.com>
2018-11-11 12:18:33 +0800

    support empty shape

--- src/index.js
+++ src/index.js
@@ -1,74 +1,74 @@
 
 let defaultConfig = {
-    delimiter:'.',
+  delimiter: '.'
 }
 let _combineReducers
 let _appliedConfig
-let convert = (leaf,prefix,delimiter)=>{
-    let reducer = leaf.reducers
-    let initState = leaf.state
-    let actionReducers = []
-    let keys = Object.getOwnPropertyNames(reducer)
-    for(let i=0;i<keys.length;i++){
-        let key = keys[i]
-        actionReducers.push({
-            actionType: prefix+delimiter+key,
-            reducer:reducer[key]
-        })
-    }
-    return (state=initState,action)=>{
-        let actionReducer = actionReducers.find((actionReducer)=>action.type==actionReducer.actionType)
-        if(actionReducer)return actionReducer.reducer(state,action)
-        return state
-    }
+let convert = (leaf, prefix, delimiter) => {
+  let reducer = leaf.reducers
+  let initState = leaf.state
+  let actionReducers = []
+  let keys = Object.getOwnPropertyNames(reducer)
+  for (let i = 0; i < keys.length; i++) {
+    let key = keys[i]
+    actionReducers.push({
+      actionType: prefix + delimiter + key,
+      reducer: reducer[key]
+    })
+  }
+  return (state = initState, action) => {
+    let actionReducer = actionReducers.find((actionReducer) => action.type === actionReducer.actionType)
+    if (actionReducer) return actionReducer.reducer(state, action)
+    return state
+  }
 }
 
-let  generateReducer = (shape,prefix,delimiter,custom)=>{
-    let keys = Object.getOwnPropertyNames(shape)
-    let combine = {}
-    for(let i=0;i<keys.length;i++){
-        let key = keys[i]
-        let property = shape[key]
-        let actualPrefix = prefix?(prefix+delimiter+key):key
-        let type = typeof property
-        if(type=='object'){
-            combine[key] = generateReducer(property,actualPrefix,delimiter)
-        }else if(type=='function'){
-            combine[key] = convert(property(),actualPrefix,delimiter)
-        }else{
-            throw new Error('unsupported property type, should be object or function')
-        }
+let generateReducer = (shape, prefix, delimiter, custom) => {
+  let keys = Object.getOwnPropertyNames(shape)
+  let combine = {}
+  for (let i = 0; i < keys.length; i++) {
+    let key = keys[i]
+    let property = shape[key]
+    let actualPrefix = prefix ? (prefix + delimiter + key) : key
+    let type = typeof property
+    if (type === 'object') {
+      combine[key] = generateReducer(property, actualPrefix, delimiter)
+    } else if (type === 'function') {
+      combine[key] = convert(property(), actualPrefix, delimiter)
+    } else {
+      throw new Error('unsupported property type, should be object or function')
     }
-    if(custom){
-        if(typeof custom!=='object'){
-            throw new Error('custom in config should be an object')
-        }
-        let keys = Object.getOwnPropertyNames(custom)
-        for(let i=0;i<keys.length;i++){
-            let key = keys[i]
-
-            let reducer = custom[key]
-            if(typeof reducer!=='function'){
-                throw new Error('custom reducer should be functions')
-            }
-            combine[key] = reducer
-        }
+  }
+  if (custom) {
+    if (typeof custom !== 'object') {
+      throw new Error('custom in config should be an object')
     }
-    return _combineReducers(combine)
+    let keys = Object.getOwnPropertyNames(custom)
+    for (let i = 0; i < keys.length; i++) {
+      let key = keys[i]
+      let reducer = custom[key]
+      if (typeof reducer !== 'function') {
+        throw new Error('custom reducer should be functions')
+      }
+      combine[key] = reducer
+    }
+  }
+  if (Object.getOwnPropertyNames(combine).length === 0) return (state, action) => state
+  return _combineReducers(combine)
 }
 
-export default (combineReducers,config)=>{
-    if(typeof combineReducers !=='function'){
-        throw new Error('combineReducers should be a function imported from redux!')
-    }
-    if(typeof config !=='object' ){
-        throw new Error('config must be an Object!')
-    }
-    if(!config['shape']){
-        throw new Error('config must contain your shape definition!')
-    }
-    _appliedConfig = Object.assign({},defaultConfig,config)
-    _combineReducers = combineReducers
-    let {shape,delimiter,custom} = _appliedConfig
-    return generateReducer(shape,'',delimiter,custom)
+export default (combineReducers, config) => {
+  if (typeof combineReducers !== 'function') {
+    throw new Error('combineReducers should be a function imported from redux!')
+  }
+  if (typeof config !== 'object') {
+    throw new Error('config must be an Object!')
+  }
+  if (!config['shape']) {
+    throw new Error('config must contain your shape definition!')
+  }
+  _appliedConfig = Object.assign({}, defaultConfig, config)
+  _combineReducers = combineReducers
+  let {shape, delimiter, custom} = _appliedConfig
+  return generateReducer(shape, '', delimiter, custom)
 }
--- test/index.js
+++ test/index.js
@@ -3,121 +3,110 @@ const reduxShape = require('../lib/index').default
 const combineReducers = require('redux').combineReducers
 const createStore = require('redux').createStore
 
-describe('#redux-shape',function(){
-    let leaf = {
-        state:"",
-        reducers:{
-            change(state,action){
-                let text = action.text
-                return text;
-            },
-            clear(state,action){
-                return '';
-            }
-        }
+describe('#redux-shape', function () {
+  let leaf = {
+    state: '',
+    reducers: {
+      change (state, action) {
+        return action.text
+      },
+      clear (state, action) {
+        return ''
+      }
     }
+  }
 
-    let mockShape =  {
-        text:()=>leaf,// a leaf should be returned inside a function.
+  let mockShape = {
+    text: () => leaf// a leaf should be returned inside a function.
+  }
+  let wrongShape = {
+    text: 1
+  }
+  it('should throw error when shape property is not a function or object', function () {
+    var fn = function () {
+      reduxShape(combineReducers, {shape: wrongShape})
     }
-    let wrongShape =  {
-        text:1,
+    expect(fn).to.throw(Error)
+  })
+  it('should throw error when combineReducers is not a function', function () {
+    var fn = function () {
+      reduxShape(null, {shape: mockShape})
     }
-    it('should throw error when shape property is not a function or object',function(){
-        var fn = function(){
-            let reducers = reduxShape(combineReducers,{shape:wrongShape})
-        }
-        expect(fn).to.throw(Error)
-    })
-    it('should throw error when combineReducers is not a function',function(){
-        var fn = function(){
-            let reducers = reduxShape(null,{shape:mockShape})
-        }
-        expect(fn).to.throw(Error)
-    })
-    it('should throw error when config is not an Object',function(){
-        var fn = function(){
-            let reducers = reduxShape(combineReducers,null)
-        }
-        expect(fn).to.throw(Error)
-    })
-    it('should throw error when config not contains \'shape\' property',function(){
-        var fn = function(){
-            let reducers = reduxShape(combineReducers,{})
-        }
-        expect(fn).to.throw(Error)
-    })
+    expect(fn).to.throw(Error)
+  })
+  it('should throw error when config is not an Object', function () {
+    var fn = function () {
+      reduxShape(combineReducers, null)
+    }
+    expect(fn).to.throw(Error)
+  })
+  it('should throw error when config not contains \'shape\' property', function () {
+    var fn = function () {
+      reduxShape(combineReducers, {})
+    }
+    expect(fn).to.throw(Error)
+  })
 
-    it('should return reducers as a function when combineReducers and config are correct',function(){
-
-            let reducers = reduxShape(combineReducers,{shape:mockShape})
-
-
-        expect(reducers).to.satisfy(function(reducers){
-            return typeof reducers === 'function'
-        })
+  it('should return reducers as a function when combineReducers and config are correct', function () {
+    let reducers = reduxShape(combineReducers, {shape: mockShape})
+    expect(reducers).to.satisfy(function (reducers) {
+      return typeof reducers === 'function'
     })
-
+  })
 })
-
-describe('#store',function(){
-    let leaf = {
-        state:"",
-        reducers:{
-            change(state,action){
-                let text = action.text
-                return text;
-            },
-            clear(state,action){
-                return '';
-            }
-        }
+describe('#store', function () {
+  let leaf = {
+    state: '',
+    reducers: {
+      change (state, action) {
+        return action.text
+      },
+      clear (state, action) {
+        return ''
+      }
     }
-
-    let mockShape =  {
-        text:()=>leaf,// a leaf should be returned inside a function.
+  }
+  let mockShape = {
+    text: () => leaf // a leaf should be returned inside a function.
+  }
+  let reducer = reduxShape(combineReducers, {
+    shape: mockShape,
+    custom: {
+      custom: (state = '', action) => {
+        if (action.custom) return action.custom
+        return state
+      }
     }
-    let reducer = reduxShape(combineReducers,{
-        shape:mockShape,
-        custom:{
-            custom:(state='',action)=>{
-                if(action.custom)return action.custom
-                return state
-            }
-        }
-    })
-    let store = createStore(reducer)
-    let dispatch = store.dispatch
-    let getState =  store.getState
+  })
+  let store = createStore(reducer)
+  let dispatch = store.dispatch
+  let getState = store.getState
 
-    it('should get correct state shape',function(){
-        let state = getState()
-        expect(state).to.satisfy((state)=>{
-            
-            return state.text == ''&&state.custom==''
-        })
+  it('should get correct state shape', function () {
+    let state = getState()
+    expect(state).to.satisfy((state) => {
+      return state.text === '' && state.custom === ''
     })
-    it('should satisfy that state changes after dispatching an action',function(){
-        dispatch({type:'text.change',text:'change text'})
-        let state = getState()
-        expect(state).to.satisfy((state)=>{
-            return state.text == 'change text'
-        })
+  })
+  it('should satisfy that state changes after dispatching an action', function () {
+    dispatch({type: 'text.change', text: 'change text'})
+    let state = getState()
+    expect(state).to.satisfy((state) => {
+      return state.text === 'change text'
     })
-    it('should satisfy that state not changes after dispatching an action that not exists',function(){
-        dispatch({type:'text.notExists',text:'I am a text'})
-        let state = getState()
-        expect(state).to.satisfy((state)=>{
-            return state.text == 'change text'
-        })
+  })
+  it('should satisfy that state not changes after dispatching an action that not exists', function () {
+    dispatch({type: 'text.notExists', text: 'I am a text'})
+    let state = getState()
+    expect(state).to.satisfy((state) => {
+      return state.text === 'change text'
     })
-    it('should get correct result when custom config is specified',function(){
-        dispatch({type:'custom',custom:'I am a text'})
-        let state = getState()
-        expect(state).to.satisfy((state)=>{
-            return state.custom == 'I am a text'
-        })
+  })
+  it('should get correct result when custom config is specified', function () {
+    dispatch({type: 'custom', custom: 'I am a text'})
+    let state = getState()
+    expect(state).to.satisfy((state) => {
+      return state.custom === 'I am a text'
     })
-
+  })
 })
-
